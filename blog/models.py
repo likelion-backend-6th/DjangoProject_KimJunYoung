d440e04ab49d320c4d602c69b0d192155ec6b4b0
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-18 15:46:06 +0900

    모델 get_absolute_url추가 slug활용으로 변경

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import CASCADE
 from django.utils import timezone
+from django.urls import reverse
 
 
 # Create your models here.
@@ -9,13 +10,14 @@ class PublishedManager(models.Manager):
 	def get_queryset(self):
 		return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
+
 class Post(models.Model):
 	class Status(models.TextChoices):
 		DRAFT = 'DF', 'Draft'
 		PUBLISHED = 'PB', 'Published'
 
 	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250)
+	slug = models.SlugField(max_length=250, unique_for_date='publish', allow_unicode=True)
 	author = models.ForeignKey(User, on_delete=CASCADE, related_name='blog_posts')
 	body = models.TextField()
 	publish = models.DateTimeField(default=timezone.now)
@@ -25,6 +27,7 @@ class Post(models.Model):
 
 	objects = models.Manager()  # 디폴트 매니저
 	published = PublishedManager()  # 사용자 정의 매니저
+
 	class Meta:
 		ordering = ['-publish']
 		indexes = [
@@ -33,5 +36,16 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse(
+			'blog:post_detail',
+			args=[
+				self.publish.year,
+				self.publish.month,
+				self.publish.day,
+				self.slug
+			]
+		)
 
 
--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,14 @@ def post_list(request):
 	return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, post_id):
-	# try:
-	# 	post = Post.published.get(id=id)
-	# except Post.DoesNotExist:
-	# 	raise Http404("No Post found.")
-	post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(
+		Post,
+		status=Post.Status.PUBLISHED,
+		slug=post,
+		publish__year=year,
+		publish__month=month,
+		publish__day=day,
+	)
 	return render(request, 'blog/post/detail.html', {'post': post})
 

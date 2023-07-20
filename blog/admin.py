ccc61ab0c38468fb9a0515d3b09cdc148121f2ba
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-20 09:32:14 +0900

    admin 페이지에 comment모델 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, Comment
+
 
 # Register your models here.
 
@@ -8,5 +9,12 @@ from blog.models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	list_display = ['title', 'slug', 'author', 'publish', 'status']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['name', 'email', 'post', 'created', 'active']
+	list_filter = ['active', 'created', 'updated']
+	search_fields = ['name', 'email', 'body']
 
 

b6d04cf3466b09ba7ed4fcbbffa143497936d088
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-20 14:30:10 +0900

    [add]post_detail뷰 similar_posts 추가

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
+from django.db.models import Count
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post
@@ -56,9 +57,17 @@ def post_detail(request, year, month, day, post):
 	comments = post.comments.filter(active=True)
 	# 댓글폼 인스턴스 생성
 	form = CommentForm()
+
+	# similar posts 리스트
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+
 	return render(request, 'blog/post/detail.html', {'post': post,
 													 'comments': comments,
-													 'form': form})
+													 'form': form,
+													 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):

6d1801992470dd16ff2729556605685655bdb603
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-20 15:51:57 +0900

    [add]사이드바에 최신글 추가

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,3 +9,9 @@ register = template.Library()
 def total_posts():
 	return Post.published.count()
 
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+	latest_posts = Post.published.order_by('-created')[:count]
+	return {'latest_posts': latest_posts}
+

dab9c043d21296d56e514aa7b471b8c24c119a83
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-20 14:16:42 +0900

    [fix]한글 에러 tag url 정규표현식으로 변경

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
 	# path('', views.post_list, name='post_list'),
 	path('', views.PostListView.as_view(), name='post_list'),
-	path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+	re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
 	# path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name='post_detail'),
 	re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
 																								 name='post_detail'),
@@ -17,3 +17,4 @@ urlpatterns = [
 ]
 
 # r'^post/(?P<slug>[-\w]+)/$'
+

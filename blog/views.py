5715d58f1a92af78f48201a51435759b9250ff3d
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-21 14:45:31 +0900

    [add]search 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
@@ -6,7 +7,7 @@ from django.views.generic import ListView
 from taggit.models import Tag
 from django.db.models import Count
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 
@@ -119,5 +120,21 @@ def post_comment(request, post_id):
 		 }
 	)
 
+
+# 검색 뷰
+def post_search(request):
+	form = SearchForm()
+	query = None
+	results = []
+	if 'query' in request.GET:
+		form = SearchForm(request.GET)
+		if form.is_valid():
+			query = form.cleaned_data['query']
+			results = Post.published.annotate(search=SearchVector('title', 'body', 'tags'),).filter(search__contains=query)
+	return render(request,
+				  'blog/post/search.html',
+				  {'form': form,
+				   'query': query,
+				   'results': results})
 
 
--- python6th_catalogapp/settings.py
+++ python6th_catalogapp/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [

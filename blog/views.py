761c788647039de0fbd82ac46dcf2323a3864d8e
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-27 16:05:45 +0900

    제출

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
@@ -130,7 +130,16 @@ def post_search(request):
 		form = SearchForm(request.GET)
 		if form.is_valid():
 			query = form.cleaned_data['query']
-			results = Post.published.annotate(search=SearchVector('title', 'body', 'tags'),).filter(search__contains=query)
+			results = Post.published.annotate(
+				similarity=TrigramSimilarity('title', query),
+			).filter(similarity__gt=0.3).order_by('-similarity')
+			# search_vector = SearchVector('title', 'tags__name', weight='A') + \
+			# 				SearchVector('body', weight='B')
+			# search_query = SearchQuery(query)
+			# results = Post.published.annotate(
+			# 	search=search_vector,
+			# 	rank=SearchRank(search_vector, search_query)
+			# ).filter(rank__gte=0.3).order_by('-rank')
 	return render(request,
 				  'blog/post/search.html',
 				  {'form': form,
